2091d93ac92ab162339a3b4b849f58993c1198a0
meanhive <meanhive@zoho.com>
2021-04-29 10:51:06 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./style.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -11,6 +13,7 @@ export default function App() {
   return (
     <Router>
       <Menu />
+      <ToastContainer />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
+import { toast } from "react-toastify";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.toastHandler = this.toastHandler.bind(this);
+  }
+
+  toastHandler = () => {
+    toast.success("Welcome to React Toast");
+  };
+
   render() {
     return (
       <div className="container mt-5">
@@ -9,6 +19,14 @@ export default class Home extends Component {
             <div className="jumbotron text-center">
               <h1 className="display-3">D.Ganesh</h1>
             </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <button onClick={this.toastHandler} className="btn btn-warning">
+              Toast
+            </button>
           </div>
         </div>
       </div>
